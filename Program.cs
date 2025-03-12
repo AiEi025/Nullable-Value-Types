1dc05d1ead2ad271c38260a146a83b5113ef8bfc
a-ehsani <a-ehsani@A-EHSANI-PC>
2025-03-12 09:09:07 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable_Value_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+            //int number = null;
+            int num; // int num = 0 ;
+
+            Nullable<int> num3 = new Nullable<int>();
+            num3 = null;
+            int? numew = num3;//همان نال ایبل بودن است
+            if (numew.HasValue)
+            {
+                Console.Write(numew);
+            }
+            else
+            {
+                Console.WriteLine(numew.GetValueOrDefault());
+            }
+            
+
+
+
+
+
+        }
+    }
+}
